83418fe2a9cf2450fc7d07627757341bff4150cb
lazerlee1990 <liam.b@outlook.com>
2022-08-07 21:37:50 +0100

    updates

---
+++ src/pages/Sub/Recruitment/index.jsx
@@ -0,0 +1,31 @@
+import React from 'react';
+import './individual.css';
+import {Container, Row, Col} from 'react-bootstrap';
+import {BreadCrumb} from '../../../components/BreadCrumb';
+
+export default function Individual() {
+    return (
+        <div className="individual-wrapper">
+            <BreadCrumb title="Recruitment"/>
+            <Container  fluid style={{width: "100%"}}>
+               {/* Full Width Container. Code Below */}
+                
+                    <Container fluid style={{width: "70%", maxWidth: "1440px"}}>
+                        {/* Restricted Container. Code Below*/}
+                        <Row>
+                            <Col className="textdescription" xxl={6} md={12}><h2>Recruitment Mockups</h2>
+                            <Container> <p>Humeka Systems GmbH,  </p> 
+                            <p></p></Container></Col>
+                        
+
+                        </Row>
+                        <Row>
+                        </Row>
+                    </Container>
+                    <Row style={{width: "100%"}}><Col className="projectdetails" xxl={6} md={12}><img src="/recruit.jpg" alt="features" className="center" /></Col></Row>
+                
+                <p></p>
+            </Container>
+        </div>
+    )
+}
